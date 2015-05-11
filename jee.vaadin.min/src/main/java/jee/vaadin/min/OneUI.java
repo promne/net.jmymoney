612b060eab14e3a0ed4ccfb165b76b29d16eda30
Jiri George Holy <nz@antiluminiscent.net>
2015-05-11 16:17:56 +1200

    version info

--- jee.vaadin.min/src/main/java/jee/vaadin/min/OneUI.java
+++ jee.vaadin.min/src/main/java/jee/vaadin/min/OneUI.java
@@ -1,18 +1,5 @@
 package jee.vaadin.min;
 
-import javax.inject.Inject;
-
-import jmm.component.LoginWindow;
-import jmm.component.LoginWindow.LoginResultType;
-import jmm.component.NavigationButton;
-import jmm.entity.UserAccount;
-import jmm.service.UserAccountService;
-import jmm.view.AccountView;
-import jmm.view.CategoryView;
-import jmm.view.PartnerView;
-import jmm.view.ReportView;
-import jmm.view.TransactionView;
-
 import com.vaadin.annotations.Theme;
 import com.vaadin.cdi.CDIUI;
 import com.vaadin.cdi.CDIViewProvider;
@@ -22,6 +9,7 @@ import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.server.Page;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinSession;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.CssLayout;
@@ -34,9 +22,23 @@ import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.BaseTheme;
 
+import javax.inject.Inject;
+
+import jmm.component.LoginWindow;
+import jmm.component.LoginWindow.LoginResultType;
+import jmm.component.NavigationButton;
+import jmm.entity.UserAccount;
+import jmm.service.UserAccountService;
+import jmm.util.ManifestUtils;
+import jmm.view.AccountView;
+import jmm.view.CategoryView;
+import jmm.view.PartnerView;
+import jmm.view.ReportView;
+import jmm.view.TransactionView;
+
 @CDIUI
-@Theme(value="jmm")
-//@PreserveOnRefresh
+@Theme(value = "jmm")
+// @PreserveOnRefresh
 public class OneUI extends UI {
 
     @Inject
@@ -47,53 +49,54 @@ public class OneUI extends UI {
 
     @Inject
     private UserAccountService userAccountService;
-    
+
     private Navigator navigator;
-    
+
     @Override
     protected void init(VaadinRequest request) {
         // check javadoc what and when this method should do
-    	if (userIdentity.getUserAccount()==null) {
-    		doLogin();
-    	} else {
-    		loadPage();
-    	}
+        if (userIdentity.getUserAccount() == null) {
+            doLogin();
+        } else {
+            loadPage();
+        }
     }
-    
-    private void doLogin() {
-		CssLayout emptyLoginLayout = new CssLayout();
-		emptyLoginLayout.setSizeFull();
-		setContent(emptyLoginLayout);
-		final LoginWindow loginWindow = new LoginWindow();
-		loginWindow.addCloseListener(closeEvent -> {
-			UserAccount userAccount = null;
-			if (loginWindow.getLoginResult().getType()==LoginResultType.LOGIN) {
-				userAccount = userAccountService.login(loginWindow.getLoginResult().getUsername(), loginWindow.getLoginResult().getPassword());
-				if (userAccount==null) {
-					Notification.show("Username or password are invalid.", Type.ERROR_MESSAGE);						
-				}				
-			} else if(loginWindow.getLoginResult().getType()==LoginResultType.REGISTER) {
-				//TODO
-			} else {
-				Notification.show("Error during login. Please try again later.", Type.ERROR_MESSAGE);
-			}
-			
-			if (userAccount==null) {
-				UI.getCurrent().addWindow(loginWindow);
-				loginWindow.focus();
-			} else {
-				userIdentity.setUserAccount(userAccount);
-				loadPage();
-				if (Page.getCurrent().getUriFragment()!=null) {
-					navigator.navigateTo(Page.getCurrent().getUriFragment());
-				}
-			}
-		});
-		UI.getCurrent().addWindow(loginWindow);						
-		loginWindow.focus();
-	}
 
-	private void loadPage() {
+    private void doLogin() {
+        CssLayout emptyLoginLayout = new CssLayout();
+        emptyLoginLayout.setSizeFull();
+        setContent(emptyLoginLayout);
+        final LoginWindow loginWindow = new LoginWindow();
+        loginWindow.addCloseListener(closeEvent -> {
+            UserAccount userAccount = null;
+            if (loginWindow.getLoginResult().getType() == LoginResultType.LOGIN) {
+                userAccount = userAccountService.login(loginWindow.getLoginResult().getUsername(),
+                        loginWindow.getLoginResult().getPassword());
+                if (userAccount == null) {
+                    Notification.show("Username or password are invalid.", Type.ERROR_MESSAGE);
+                }
+            } else if (loginWindow.getLoginResult().getType() == LoginResultType.REGISTER) {
+                // TODO
+            } else {
+                Notification.show("Error during login. Please try again later.", Type.ERROR_MESSAGE);
+            }
+
+            if (userAccount == null) {
+                UI.getCurrent().addWindow(loginWindow);
+                loginWindow.focus();
+            } else {
+                userIdentity.setUserAccount(userAccount);
+                loadPage();
+                if (Page.getCurrent().getUriFragment() != null) {
+                    navigator.navigateTo(Page.getCurrent().getUriFragment());
+                }
+            }
+        } );
+        UI.getCurrent().addWindow(loginWindow);
+        loginWindow.focus();
+    }
+
+    private void loadPage() {
         HorizontalSplitPanel pageLayout = new HorizontalSplitPanel();
         pageLayout.setSplitPosition(200, Unit.PIXELS);
         pageLayout.setSizeFull();
@@ -161,6 +164,10 @@ public class OneUI extends UI {
         footerLayout.setMargin(true);
         footerLayout.setSpacing(true);
         
+        Label labelLoggedAs = new Label(String.format("You are logged as \"%s\"", userIdentity.getUserAccount().getUsername()));
+        footerLayout.addComponent(labelLoggedAs);
+        footerLayout.setComponentAlignment(labelLoggedAs, Alignment.MIDDLE_CENTER);
+        
         Button logoutButton = new Button("logout");
         logoutButton.addStyleName(BaseTheme.BUTTON_LINK);
         logoutButton.addClickListener(listener -> {
@@ -173,9 +180,14 @@ public class OneUI extends UI {
             getSession().close();
             getSession().getSession().invalidate();
         });
-        footerLayout.addComponent(new Label(String.format("You are logged as \"%s\"", userIdentity.getUserAccount().getUsername())));
         footerLayout.addComponent(logoutButton);
         
+        
+        String version = ManifestUtils.getVersion();
+        if (version != null) {
+            labelLoggedAs.setDescription(String.format("Version %s", version));
+        }
+              
         wholePage.addComponent(footerLayout);
         
         setContent(wholePage);    	
