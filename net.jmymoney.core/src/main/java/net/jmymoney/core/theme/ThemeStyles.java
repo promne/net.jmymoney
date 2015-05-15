408a331fe9560a77ced16cb9348aa67cdd2ac578
Jiri George Holy <nz@antiluminiscent.net>
2015-05-15 14:06:28 +1200

    responsive

---
+++ net.jmymoney.core/src/main/java/net/jmymoney/core/theme/ThemeStyles.java
@@ -0,0 +1,25 @@
+package net.jmymoney.core.theme;
+
+public final class ThemeStyles {
+
+    public static final String MENU_VISIBLE = "valo-menu-visible";
+
+    public static final String MENU_TOGGLE = "valo-menu-toggle";
+
+    public static final String MENU_ITEM = "valo-menu-item";
+
+    public static final String MENU_ITEMS = "valo-menuitems";
+
+    public static final String MENU_TITLE = "valo-menu-title";
+
+    public static final String MENU_PART = "valo-menu-part";
+
+    public static final String CONTENT = "valo-content";
+    
+    public static final String SCROLLABLE = "v-scrollable";
+
+    public static final String USER_MENU = "user-menu";
+
+    public static final String SELECTED = "selected";
+    
+}
